7a57f64a314e03c748a1583393f43beaa2335db0
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2024-03-02 22:47:58 +0500

    Project Setup

---
+++ wp-content/plugins/wp-restapi-auth/includes/class-wp-restapi-auth-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/cod3pk
+ * @since      1.0.0
+ *
+ * @package    Wp_Restapi_Auth
+ * @subpackage Wp_Restapi_Auth/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Restapi_Auth
+ * @subpackage Wp_Restapi_Auth/includes
+ * @author     cod3pk <asfandkhan404@gmail.com>
+ */
+class Wp_Restapi_Auth_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
